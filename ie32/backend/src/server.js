ff578deee9882ae80b113d0383a4883a8653f991
Forest Lou <jlou0008@student.monash.edu>
2024-08-28 21:43:34 +1000

    cd v8

--- ie32/backend/src/server.js
+++ ie32/backend/src/server.js
@@ -54,6 +54,7 @@ app.get('/api/benchmark-vic', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 console.log('PORT:', PORT);
+console.log('PORT:', PORT);
 // Check environment and use HTTPS in production
 if (process.env.NODE_ENV === 'production') {
     const httpsOptions = {
