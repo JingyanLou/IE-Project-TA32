1c909e24fc9a54e0cb23b91ca4bd40f3b78cb029
Forest Lou <jlou0008@student.monash.edu>
2024-09-08 19:15:38 +1000

    Homepage UI Responsive fix...

--- ie32/frontend/src/pages/homepage.js
+++ ie32/frontend/src/pages/homepage.js
@@ -24,8 +24,8 @@ const Homepage = () => {
                         position: 'absolute',
                         top: 0,
                         left: 0,
-                        width: '100vw',
-                        height: '100vh',
+                        width: '100%',
+                        height: '100%',
                         pointerEvents: 'none',
                     }}
                 >
