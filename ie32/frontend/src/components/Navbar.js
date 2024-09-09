30939a7e51dedfa44bf9cd17e07f5952e9c20c40
Forest Lou <jlou0008@student.monash.edu>
2024-09-09 17:54:30 +1000

    Features/Aboutus added in navbar, features drop down menu added for navigation

--- ie32/frontend/src/components/Navbar.js
+++ ie32/frontend/src/components/Navbar.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link for navigation to Home page
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
         <nav className="navbar">
             <ul className="brand">
-                <li><span>EnergiseSmart</span></li> {/* Brand or title of the website */}
+                <li><Link to="/">EnergiseSmart</Link></li>
             </ul>
             <ul className="nav-items">
-                {/* Link to the homepage */}
                 <li><Link to="/">Home</Link></li>
-
-                {/* Placeholder links with no actual navigation */}
-
+                <li
+                    className={`dropdown ${isDropdownOpen ? 'active' : ''}`}
+                    onMouseEnter={() => setIsDropdownOpen(true)}
+                    onMouseLeave={() => setIsDropdownOpen(false)}
+                >
+                    <span onClick={toggleDropdown}>Features</span>
+                    {isDropdownOpen && (
+                        <ul className="dropdown-menu">
+                            <li><Link to="/estimation-introduction">Estimation</Link></li>
+                            <li><Link to="/selection">Energy Saving Tips</Link></li>
+                        </ul>
+                    )}
+                </li>
+                <li><Link to="/">About us</Link></li>
             </ul>
         </nav>
     );
