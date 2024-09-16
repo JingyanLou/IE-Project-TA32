beee68086efdb2de7ee0c925726e631431c6061d
Forest Lou <jlou0008@student.monash.edu>
2024-09-16 19:40:59 +1000

    Savingtips admentation

--- ie32/frontend/src/components/Savingtips.js
+++ ie32/frontend/src/components/Savingtips.js
@@ -43,14 +43,16 @@ const SavingTips = () => {
                 <div className="content">
                     <h1>Uncover Energy-Saving Secrets and Transform Your Home</h1>
                     <div className="stats">
+
                         <div className="stat-item">
-                            <h2>Cut Annual Energy</h2>
-                            <p>30% Lower</p>
+                            <h2>Access Government Rebates</h2>
+                            <p>Up to $500 Savings</p>
                         </div>
                         <div className="stat-item">
-                            <h2>Reduce Consumption</h2>
-                            <p>20% Lower</p>
+                            <h2>Upgrade to Energy-Efficient Appliances</h2>
+                            <p>20% Lower Energy Bills</p>
                         </div>
+
                     </div>
                     <p className="description">Learn how to optimise your current appliances for better efficiency and sustainability. Find out how replacing outdated, inefficient models with star-rated devices, along with leveraging government support, can reduce your consumption and lower your carbon footprint.</p>
                     <button className="discover-button" onClick={handleDiscoverClick}>
