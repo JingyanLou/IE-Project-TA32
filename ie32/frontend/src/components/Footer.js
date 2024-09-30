6de09ed2029296b21edf1725494d5f82510ab327
Forest Lou <jlou0008@student.monash.edu>
2024-09-30 16:20:30 +1000

    footer brand style adjustment, trademarks added

--- ie32/frontend/src/components/Footer.js
+++ ie32/frontend/src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
             .to(footerContainer, { yPercent: 0, ease: 'none' });
 
         ScrollTrigger.create({
-            trigger: '#saving-tips', // Assuming this is your "conclusion" section
+            trigger: '#saving-tips',
             start: 'top top',
             end: '+=75%',
             animation: uncover,
@@ -32,21 +32,25 @@ const Footer = () => {
     return (
         <footer className="footer-footer">
             <section ref={footerContainerRef} className="footer-container-footer">
-                <h2 className="brand-title-footer">EnergiseSmart®</h2>
-                <div className="feature-section-footer">
-                    <h3 className="feature-title-footer">Estimate your home electricity bill</h3>
-                    <ul className="feature-list-footer">
-                        <li className="feature-item-footer">Start now</li>
-                        <li className="feature-item-footer">Estimation user guide</li>
-                    </ul>
-                </div>
-                <div className="feature-section-footer">
-                    <h3 className="feature-title-footer">Explore more on home electricity saving tips</h3>
-                    <ul className="feature-list-footer">
-                        <li className="feature-item-footer">Replace your appliances</li>
-                        <li className="feature-item-footer">Appliances saving tips</li>
-                        <li className="feature-item-footer">Government programs</li>
-                    </ul>
+                <div className="footer-content-footer">
+                    <h2 className="brand-title-footer">Energise<br />Smart®</h2>
+                    <div className="footer-features-footer">
+                        <div className="feature-section-footer">
+                            <h3 className="feature-title-footer">Estimate your home electricity bill</h3>
+                            <ul className="feature-list-footer">
+                                <li className="feature-item-footer">Start now</li>
+                                <li className="feature-item-footer">Estimation user guide</li>
+                            </ul>
+                        </div>
+                        <div className="feature-section-footer">
+                            <h3 className="feature-title-footer">Explore more on home electricity saving tips</h3>
+                            <ul className="feature-list-footer">
+                                <li className="feature-item-footer">Replace your appliances</li>
+                                <li className="feature-item-footer">Appliances saving tips</li>
+                                <li className="feature-item-footer">Government programs</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </footer>
